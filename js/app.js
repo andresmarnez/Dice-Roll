13349686727980a3bc0da7b1f141f5464fbbdb3e
Andrés Sánchez <andresmarnez@gmail.com>
2020-09-30 00:17:22 +0200

    Images and Functions
    Updated functions:
    
    - minus()
    - more()

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /**
- * This function will genberate a random number and update de html
+ * This function will generate a random number and update de html
  * based on how many faces does the die contain.
  *
  * @param {number} face - A number
@@ -7,29 +7,53 @@
  *
  * @example
  * roll(20);
- *
+ * https://devhints.io/jsdoc
  */
 
 const roll = () => {
   var result = Math.floor(Math.random()*dice[actualface] + 1);
   let texto = document.querySelector("main");
+  let log = document.getElementById("history");
 
   if(texto != null){
     texto.innerHTML = `<h1>${result}</h1>`;
-    console.log(result);
+  }
+  else throw console.error(`Error with the main HTML object, main = " ${texto} "`);
+
+  if(log != null){
+
+    var inner = log.innerText;
+    if (inner === "" || inner.length > 28) log.innerText = `${result}`;
+    else log.innerText = `${inner}, ${result}`;
     return true;
   }
-
-  else throw console.error(`Error with the main HTML object, main = " ${texto} "`);
+  else throw console.error(`Error with the main HTML object, main = " ${log} "`);
 }
 
+/**
+ * This function will change de dice we are currently rolling in both js and html img.
+ *
+ * @example
+ * minus();
+ *
+ */
 const minus = () => {
   actualface--;
 
   if(actualface<0){
     actualface=dice.length - 1;
   }
+  const img = document.getElementById("die");
+  img.src = diceimgurl[actualface];
 }
+
+/**
+ * This function will change de dice we are currently rolling in both js and html img.
+ *
+ * @example
+ * more();
+ *
+ */
 
 const more = () => {
   actualface++;
@@ -37,14 +61,18 @@ const more = () => {
   if (actualface>4) {
     actualface = 0;
   }
+  const img = document.getElementById("die");
+  img.src = diceimgurl[actualface];
 }
 
 /**
  * These global variables will indicate on which die the user is situated.
  *
  * @let {number}
- * @let {array [number,number,number,number,number]}
+ * @const {array [number,number,number,number,number]}
+ * @const {array [string,string,string,string,string]}
  */
 
 let actualface = 4;
-let dice = ["4","6","8","12","20"];
+const dice = ["4","6","8","12","20"];
+const diceimgurl = ["img/d4.png","img/d6.png","img/d20.png","img/d20.png","img/d20.png"];
